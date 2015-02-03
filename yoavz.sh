c5e718008e51dc456023a795ef8e4872b1243a7b
Yoav Zimmerman <yoavzimmerman@gmail.com>
2015-02-02 19:22:36 -0800

    merge

--- yoavz.sh
+++ yoavz.sh
@@ -1,2 +1,2 @@
 ./tools/sbt-tracker 60207 ./tools/test-2.torrent > test.result & 
-./tools/sbt-peer 11111 ./tools/test-2.torrent ./tools/ SIMPLEBT.TEST.111111 > peer.result &
+./tools/sbt-peer 11111 ./tools/test-2.torrent ./tools/ SIMPLEBT.TEST.111111 2>/dev/null &
