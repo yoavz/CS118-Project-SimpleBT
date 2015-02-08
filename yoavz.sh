85281bd58126a8de037abbf7ad7396f4d4361c51
Yoav Zimmerman <yoavzimmerman@gmail.com>
2015-02-08 13:10:25 -0800

    updated testing script

--- yoavz.sh
+++ yoavz.sh
@@ -1,2 +1,9 @@
 ./tools/sbt-tracker 60207 ./tools/test-2.torrent > test.result & 
+
+sleep 1
+
 ./tools/sbt-peer 11111 ./tools/test-2.torrent ./tools/ SIMPLEBT.TEST.111111 2>/dev/null &
+
+sleep 1
+
+./build/simple-bt 60207 ./tools/test-2.torrent 
